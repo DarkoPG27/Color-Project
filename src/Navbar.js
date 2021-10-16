22b0e131577a970ede0b1fecdd6f22c49790e1ff
DarkoPG27 <darkobrnovic27@gmail.com>
2021-10-16 10:48:40 +0200

    Refactor NavbarStyles and remove CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@mui/styles';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-
 import Slider from 'rc-slider';
+
+import styles from './styles/NavbarStyles';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
 
 class Navbar extends Component {
     constructor(props) {
@@ -28,30 +29,31 @@ class Navbar extends Component {
     }
 
     render() {
-        const { level, changeLevel } = this.props;
+        const { level, changeLevel, showingAllColors, classes } = this.props;
         const { format } = this.state;
 
         return (
-            <header className="Navbar">
-                <div className="logo">
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
                     <Link to="/">
                         reactcolorpicker
                     </Link>
                 </div>
-                {this.props.showingAllColors && (<div className="slider-container">
-                    <span>Level: {level}</span>
-                    <div className="slider">
-                        <Slider
-                            defaultValue={level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={changeLevel}
-                        />
+                {showingAllColors && (
+                    <div>
+                        <span>Level: {level}</span>
+                        <div className={classes.slider}>
+                            <Slider
+                                defaultValue={level}
+                                min={100}
+                                max={900}
+                                step={100}
+                                onAfterChange={changeLevel}
+                            />
+                        </div>
                     </div>
-                </div>
                 )}
-                <div className="select-container">
+                <div className={classes.selectContainer}>
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff </MenuItem>
                         <MenuItem value="rgb">RGB -  rgb(255,255,255)</MenuItem>
@@ -82,4 +84,4 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
