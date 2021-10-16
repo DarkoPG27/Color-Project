c5dd8fc04020dfb33252e384a44c5d260ccfd86f
DarkoPG27 <darkobrnovic27@gmail.com>
2021-10-16 10:11:02 +0200

    Move Out Styles

--- src/Palette.js
+++ src/Palette.js
@@ -3,21 +3,7 @@ import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFooter';
 import { withStyles } from '@mui/styles';
 import Navbar from './Navbar';
-import './Palette.css';
-
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-
-    colors: {
-        height: "90%"
-    }
-
-
-}
+import styles from './styles/PaletteStyles';
 
 class Palette extends Component {
     constructor(props) {
